3e0da58b461487e81d4ccbac0e9ec124d0c9458f
Brayden <babick@lakeheadu.ca>
2018-06-08 21:14:41 +0200

    Sublime Text Fix
    Sublime Text Fix

---
+++ parts/footer.php
@@ -0,0 +1,8 @@
+<footer>
+<div class="container">
+<p>Footer Content</p>
+</div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="/assets/scripts/bootstrap.min.js"></script>
+
